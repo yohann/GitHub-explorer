16201d7c60b784188bbc3713a0a789862964c38c
Yohann <canoyh7@gmail.com>
2017-12-26 13:19:18 -0200

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>teste funciona</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  entry: {
+    app: './app/App.js'
+  },
+
+  output: {
+    filename: 'public/bundle.js',
+  },
+
+  module: {
+    loaders: [       
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ],
+  }
+};
